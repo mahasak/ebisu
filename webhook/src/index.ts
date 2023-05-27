ffe25e6a612317d9dac419143578f7263a282994
Mahasak Pijittum <mahasak@gmail.com>
2023-05-27 20:39:59 +0800

    start webhook server

--- webhook/src/index.ts
+++ webhook/src/index.ts
@@ -1,6 +1,23 @@
 import fastify from 'fastify'
 
-const server = fastify()
+const mode = process.env.mode ? "prod" : "dev";
+const logOptions = {
+  dev: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+  prod: true,
+  test: false,
+}
+const environment = "dev"
+
+const server = fastify({
+  logger: logOptions[mode] ?? true })
 
 server.get('/ping', async (request, reply) => {
   return 'ponsg\n'
